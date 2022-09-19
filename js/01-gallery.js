b72e5c742e62ce928ea873327761f91558666dbe
Ellena83 <abel2000@ukr.net>
2022-09-19 19:14:18 +0200

    homework js 7
    homework js 7 is ready

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galeryListRef = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+        })
+        .join('');
+}
+const imageGallery = createGallery(galleryItems);
+galeryListRef.insertAdjacentHTML("afterbegin", imageGallery);
+
+galeryListRef.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const source = event.target.dataset.source;    
+    const instance = basicLightbox.create(`
+   <div class ='modal'></div>
+    <img src="${source}" width="800" height="600">
+ 
+`);
+    instance.show();
+    window.addEventListener("keydown", (event) => {
+        event.preventDefault();
+            if (event.code === "Escape") {
+            instance.close();
+        }
+        
+    })
+}
+
+
+
+    
+
+
+
+
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const ulRef = document.querySelector('.gallery');
+
+function createGallery(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+             src="${preview}" 
+             alt="${description}" />
+                    </a>`;
+        })
+        .join('');
+}
+const imageGallery = createGallery(galleryItems);
+ulRef.insertAdjacentHTML("afterbegin", imageGallery);
+
+ulRef.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captions: 'true',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: '250'
+}); 
+}
